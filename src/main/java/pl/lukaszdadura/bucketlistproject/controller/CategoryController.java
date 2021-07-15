53dc95cafefcbeee39ff719a4cb4997b9f527226
lukaszdadura <83578617+lukaszdadura@users.noreply.github.com>
2021-07-15 10:03:16 +0200

    small changes

--- src/main/java/pl/lukaszdadura/bucketlistproject/controller/CategoryController.java
+++ src/main/java/pl/lukaszdadura/bucketlistproject/controller/CategoryController.java
@@ -2,7 +2,7 @@ package pl.lukaszdadura.bucketlistproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.lukaszdadura.bucketlistproject.model.Category;
 import pl.lukaszdadura.bucketlistproject.service.CategoryService;
 
@@ -22,5 +22,19 @@ public class CategoryController {
         List<Category> categoryList = categoryService.findAllCategories();
         model.addAttribute("categoryList", categoryList);
         return "categoryList";
+    }
+
+    @GetMapping("/categoryadd")
+    public String getCategoryAdd() {
+        return "categoryAdd";
+    }
+
+    @RequestMapping(value = "/categoryadd", method = RequestMethod.POST)
+    public String postCategoryAdd(@RequestParam String points, @RequestParam String type) {
+        Category category = new Category();
+        category.setPoints(Integer.parseInt(points));
+        category.setType(type);
+        categoryService.addCategory(category);
+        return "redirect:/categories";
     }
 }
