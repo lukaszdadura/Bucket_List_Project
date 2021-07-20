866605761a32dc89258098c24b22054ff442599e
lukaszdadura <83578617+lukaszdadura@users.noreply.github.com>
2021-07-20 15:59:23 +0200

    dziala lista w formularzu achievementu nie dziala data

--- src/main/java/pl/lukaszdadura/bucketlistproject/controller/UserAchievementController.java
+++ src/main/java/pl/lukaszdadura/bucketlistproject/controller/UserAchievementController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import pl.lukaszdadura.bucketlistproject.model.Achievement;
 import pl.lukaszdadura.bucketlistproject.model.User;
 import pl.lukaszdadura.bucketlistproject.model.UserAchievement;
+import pl.lukaszdadura.bucketlistproject.repository.UserRepository;
 import pl.lukaszdadura.bucketlistproject.service.AchievementService;
 import pl.lukaszdadura.bucketlistproject.service.UserAchievementService;
 import pl.lukaszdadura.bucketlistproject.service.UserService;
@@ -22,12 +23,14 @@ public class UserAchievementController {
     private final UserAchievementService userAchievementService;
     private final UserService userService;
     private final AchievementService achievementService;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserAchievementController(UserAchievementService userAchievementService, UserService userService, AchievementService achievementService) {
+    public UserAchievementController(UserAchievementService userAchievementService, UserService userService, AchievementService achievementService, UserRepository userRepository) {
         this.userAchievementService = userAchievementService;
         this.userService = userService;
         this.achievementService = achievementService;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/user/showachievements")
@@ -43,13 +46,16 @@ public class UserAchievementController {
     public String getUserAchievementAdd(Model model) {
         UserAchievement userAchievement = new UserAchievement();
         List<Achievement> achievementList = achievementService.findAllAchievements();
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByEmail(principal.toString());
+        model.addAttribute("user", user);
         model.addAttribute("achievementlist", achievementList);
         model.addAttribute("userachievement", userAchievement);
         return "user/userAchievementAdd";
     }
 
     @PostMapping("/user/addachievements")
-    public String getUserAchievementAdd(@ModelAttribute("userachievement") UserAchievement userAchievement) {
+    public String postUserAchievementAdd(@ModelAttribute("userachievement") UserAchievement userAchievement) {
         userAchievementService.addUserAchievement(userAchievement);
         return "redirect:/user/achievementmanage";
     }
