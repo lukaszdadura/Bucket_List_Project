7d35d46d161a08cb10171da92cdd20e426e62b0a
lukaszdadura <83578617+lukaszdadura@users.noreply.github.com>
2021-07-21 22:46:28 +0200

    a lot

--- src/main/java/pl/lukaszdadura/bucketlistproject/controller/UserController.java
+++ src/main/java/pl/lukaszdadura/bucketlistproject/controller/UserController.java
@@ -7,9 +7,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.lukaszdadura.bucketlistproject.model.Achievement;
+import pl.lukaszdadura.bucketlistproject.model.Category;
 import pl.lukaszdadura.bucketlistproject.model.User;
+import pl.lukaszdadura.bucketlistproject.model.UserAchievement;
+import pl.lukaszdadura.bucketlistproject.repository.UserAchievementRepository;
 import pl.lukaszdadura.bucketlistproject.repository.UserRepository;
 import pl.lukaszdadura.bucketlistproject.service.AchievementService;
+import pl.lukaszdadura.bucketlistproject.service.CategoryService;
 import pl.lukaszdadura.bucketlistproject.service.UserAchievementService;
 import pl.lukaszdadura.bucketlistproject.service.UserService;
 
@@ -23,13 +27,17 @@ public class UserController {
     private final AchievementService achievementService;
     private final UserAchievementService userAchievementService;
     private final UserRepository userRepository;
+    private final CategoryService categoryService;
+    private final UserAchievementRepository userAchievementRepository;
 
     @Autowired
-    public UserController(UserService userService, AchievementService achievementService, UserAchievementService userAchievementService, UserRepository userRepository) {
+    public UserController(UserService userService, AchievementService achievementService, UserAchievementService userAchievementService, UserRepository userRepository, CategoryService categoryService, UserAchievementRepository userAchievementRepository) {
         this.userService = userService;
         this.achievementService = achievementService;
         this.userAchievementService = userAchievementService;
         this.userRepository = userRepository;
+        this.categoryService = categoryService;
+        this.userAchievementRepository = userAchievementRepository;
     }
 
     @GetMapping("/home")
@@ -42,6 +50,34 @@ public class UserController {
     @GetMapping("/achievementmanage")
     public String achievementManage() {
         return "user/achievementManage";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "user/userLogin";
+    }
+
+
+    @GetMapping("/signup")
+    public String signUp(Model model) {
+        model.addAttribute("user", new User());
+        return "user/userSignup";
+    }
+
+    @GetMapping("/categories")
+    public String categoryList(Model model) {
+        List<Category> categoryList = categoryService.findAllCategories();
+        model.addAttribute("categoryList", categoryList);
+        return "user/userCategoryList";
+    }
+
+    @GetMapping("/achievements")
+    public String achievementList(Model model) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByEmail(principal.toString());
+        List<UserAchievement> userAchievementList = userAchievementRepository.findAllByUserId(user.getId());
+        model.addAttribute("userachievementlist", userAchievementList);
+        return "user/userAchievementList";
     }
 
 }
