43ba0f60449f59d8c769d49f870112485bf1d777
lukaszdadura <83578617+lukaszdadura@users.noreply.github.com>
2021-07-22 09:02:08 +0200

    final changes

--- src/main/java/pl/lukaszdadura/bucketlistproject/controller/AchievementController.java
+++ src/main/java/pl/lukaszdadura/bucketlistproject/controller/AchievementController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.lukaszdadura.bucketlistproject.model.Achievement;
+import pl.lukaszdadura.bucketlistproject.model.Category;
 import pl.lukaszdadura.bucketlistproject.service.AchievementService;
+import pl.lukaszdadura.bucketlistproject.service.CategoryService;
 
 import java.util.List;
 
@@ -13,10 +17,12 @@ import java.util.List;
 public class AchievementController {
 
     private final AchievementService achievementService;
+    private final CategoryService categoryService;
 
     @Autowired
-    public AchievementController(AchievementService achievementService) {
+    public AchievementController(AchievementService achievementService, CategoryService categoryService) {
         this.achievementService = achievementService;
+        this.categoryService = categoryService;
     }
 
     @GetMapping("/achievements")
@@ -24,5 +30,20 @@ public class AchievementController {
         List<Achievement> achievementList = achievementService.findAllAchievements();
         model.addAttribute("achievementList", achievementList);
         return "achievementList";
+    }
+
+    @GetMapping("/achievementadd")
+    public String getAchievementAdd(Model model) {
+        Achievement achievement = new Achievement();
+        List<Category> categoryList = categoryService.findAllCategories();
+        model.addAttribute("achievement", achievement);
+        model.addAttribute("categorylist", categoryList);
+        return "admin/achievementAdd";
+    }
+
+    @PostMapping("/achievementadd")
+    public String postAchievementAdd(@ModelAttribute("achievement") Achievement achievement) {
+        achievementService.addAchievement(achievement);
+        return "redirect:/achievements";
     }
 }
